615c0807bc87df4f890c464309d0a6423edb02a7
sunny <anna7789@naver.com>
2025-05-12 13:29:00 +0900

    기존의 main 삭제

--- app/main_gpu.py
+++ app/main_gpu.py
@@ -14,4 +14,4 @@ async def healthz():
     return {"status": "ok", "message": "API is running"}
 
 if __name__ == "__main__":
-    uvicorn.run("app.main:app", host="0.0.0.0", port=8080, reload=True)
+    uvicorn.run("app.main_gpu:app", host="0.0.0.0", port=8080, reload=True)
