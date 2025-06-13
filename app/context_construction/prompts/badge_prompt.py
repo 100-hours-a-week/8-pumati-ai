c1c7a185e3a2dac9b877e2c707037865a0560356
HBPark <HBPark@example.com>
2025-06-13 10:33:29 +0900

    fix: [badge]WDEAddoptions

--- app/context_construction/prompts/badge_prompt.py
+++ app/context_construction/prompts/badge_prompt.py
@@ -127,14 +127,24 @@ class BadgePrompt:
         # options.add_argument("--disable-gpu")  # GPU 비활성화 (optional)
         # #options.add_argument("--single-process")
         # options.add_argument("--remote-debugging-port=9222")  # DevToolsActivePort 에러 방지
+        
         # chrome_options = webdriver.ChromeOptions()
         # chrome_options.add_argument('--headless')
         # chrome_options.add_argument('--no-sandbox')
         # chrome_options.add_argument('--disable-dev-shm-usage')
-        options.add_argument("--headless")
-        options.add_argument("--no-sandbox")
-        options.add_argument("--disable-dev-shm-usage")
-        options.add_argument("--disable-gpu")
+
+        # options.add_argument("--headless")
+        # options.add_argument("--no-sandbox")
+        # options.add_argument("--disable-dev-shm-usage")
+        # options.add_argument("--disable-gpu")
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
+        options.add_argument('--disable-gpu')
+        options.add_argument('--disable-software-rasterizer')
+        options.add_argument('--window-size=1920,1080')
+        options.add_argument('--remote-debugging-port=9222')
+        options.add_argument('--no-zygote')
 
         logger.info("4-5) 크롬 트라이버 생성중...")
         with tempfile.TemporaryDirectory() as user_data_dir:
